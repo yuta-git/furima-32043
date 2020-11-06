6d4f342d0abac2aa61b9957cd354f9f001a8495e
yuta-git <64824509+yuta-git@users.noreply.github.com>
2020-11-06 21:08:58 +0900

    itemコントローラの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -45,7 +45,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    redirect_to action: :index unless user_signed_in? && current_user.id == item.user_id
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
