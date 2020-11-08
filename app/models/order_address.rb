e2cfc501a1c1727cf97fe636f132b04ff31b8957
yuta-git <64824509+yuta-git@users.noreply.github.com>
2020-11-08 11:45:15 +0900

    売却済みの商品のリダイレクト実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,14 +3,13 @@ class OrderAddress
   attr_accessor :zip, :prefecture_id, :city, :block, :building, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
-    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :zip, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
     validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width numbers.' }
     validates :token
   end
-
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
